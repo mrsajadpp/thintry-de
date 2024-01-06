3a4eb43529dbae45ff4894150e2ceea162e51652
mrsajadpp <saju4753@gmail.com>
2024-01-06 20:07:15 +0530

    body

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+// const video = document.getElementById('bgVid');
+
+
+// window.addEventListener('mousemove', (event) => {
+//     // Get the mouse X position relative to the video element
+//     const mouseX = event.offsetX / video.clientWidth;
+
+//     // Set the video duration based on mouse X position
+//     video.currentTime = mouseX * video.duration;
+// });
